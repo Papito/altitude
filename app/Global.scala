23b12d5ce82cae6a509c26959f276aa0fc735955
papito <a.taranchenko@yahoo.com>
2014-10-21 19:39:08 -0400

    The manager is the default.

--- app/Global.scala
+++ app/Global.scala
@@ -8,26 +8,26 @@ object Global extends GlobalSettings {
 	private def getSubdomain (request: RequestHeader) = request.domain.replaceFirst("[\\.]?[^\\.]+[\\.][^\\.]+$", "")
 	
 	override def onRouteRequest (request: RequestHeader) = getSubdomain(request) match {
-		case "manager" => manager.Routes.routes.lift(request)
-		case _ => client.Routes.routes.lift(request)
+		case "web" => client.Routes.routes.lift(request)
+		case _ => manager.Routes.routes.lift(request)
 	}
 	
 	// 404 - page not found error
 	override def onHandlerNotFound (request: RequestHeader) = getSubdomain(request) match {
-		case "manager" => ManagerGlobal.onHandlerNotFound(request)
-		case _ => ClientGlobal.onHandlerNotFound(request)
+		case "web" => ClientGlobal.onHandlerNotFound(request)
+		case _ => ManagerGlobal.onHandlerNotFound(request)
 	}
 	
 	// 500 - internal server error
 	override def onError (request: RequestHeader, throwable: Throwable) = getSubdomain(request) match {
-		case "manager" => ManagerGlobal.onError(request, throwable)
-		case _ => ClientGlobal.onError(request, throwable)
+		case "web" => ClientGlobal.onError(request, throwable)
+		case _ => ManagerGlobal.onError(request, throwable)
 	}
 	
 	// called when a route is found, but it was not possible to bind the request parameters
 	override def onBadRequest (request: RequestHeader, error: String) = getSubdomain(request) match {
-		case "manager" => ManagerGlobal.onBadRequest(request, error)
-		case _ => ClientGlobal.onBadRequest(request, error)
+		case "web" => ClientGlobal.onBadRequest(request, error)
+		case _ => ManagerGlobal.onBadRequest(request, error)
 	}
 
 }
