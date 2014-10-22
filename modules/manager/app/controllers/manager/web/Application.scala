e0e40dd8a74c969b18ba1cc2ca3f7be322a6b1e2
papito <a.taranchenko@yahoo.com>
2014-10-22 18:27:51 -0400

    Stripping stuff.

--- modules/manager/app/controllers/manager/web/Application.scala
+++ modules/manager/app/controllers/manager/web/Application.scala
@@ -8,7 +8,7 @@ import play.api.Play.current
 object Application extends Controller {
 
 	def index = Action { implicit request =>
-		Ok(views.html.manager.index("Hello! I'm the MANAGER!"))
+		Ok(views.html.manager.index())
 	}
 
 }
