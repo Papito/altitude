030c977ec5fdfffc612e36aa13a0bba8caebd105
papito <a.taranchenko@yahoo.com>
2014-10-23 00:45:40 -0400

    API: Stubj json response.

--- modules/manager/app/controllers/manager/api/Root.scala
+++ modules/manager/app/controllers/manager/api/Root.scala
@@ -2,11 +2,14 @@ package controllers.manager.api
 
 import models._
 import play.api._
+import play.api.libs.json._
 import play.api.mvc._
 import play.api.Play.current
 
 object Root extends Controller {
 
-    def index = Action { implicit request => Ok("") }
+    def index = Action { implicit request => 
+        Ok( Json.obj("assets" -> List[String]()) )
+    }
 
 }
