7d8cefaba40e021e00631b81d179359d8286007d
papito <a.taranchenko@yahoo.com>
2014-10-25 19:05:51 -0400

    Beginnings of a modest json logger.

--- modules/common/app/util/Log.scala
+++ modules/common/app/util/Log.scala
@@ -1,7 +1,28 @@
-package util 
+package util
+import play.api.libs.json._
+import java.util.logging.Level
 
 object log {
 
-  def info(msg: String, v: Map[String, String], t: String*) {}
-  def info(msg: String, t: String*) {}
+  def info(msg: String, v: Map[String, String], t: String*): Unit = {
+    this.log(Level.INFO, msg = msg, v = v, t = t)
+  }
+
+  def info(msg: String, t: String*): Unit = {
+    this.log(Level.INFO, msg = msg, t = t)
+  }
+
+  private def log(level: Level, msg: String, v: Map[String, String] = Map[String, String](), t: Seq[String] = Seq[String]()): Unit = {
+    val stm = this.getLogStmt(level = level, msg = msg, v = v, t = t)
+    println(stm)
+  }
+
+  def getLogStmt(level: Level, msg: String, v: Map[String, String] = Map[String, String](), t: Seq[String] = Seq[String]()) = {
+    val json: JsValue = JsObject(Seq(
+      "msg" -> JsString(msg),
+      "level" -> JsString(level.toString)
+    ))
+
+    Json.stringify(json)
+  }
 }
--- modules/common/test/JayLogTests.scala
+++ modules/common/test/JayLogTests.scala
@@ -3,9 +3,8 @@ import util.log
 
 class JayLogTests extends FunSuite {
 
-  test("An empty Set should have size 0") {
+  test("Log messages should, uh, work") {
     val v = Map[String, String]("v1" -> "v1", "v2" -> "v2")
-    val t = Set[String]("tag1", "tag2")
 
     log.info("message")
     log.info("message", v)
