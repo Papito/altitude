f5d83eb89cae2f104a06d0c3eae6dfb1fce5660b
papito <a.taranchenko@yahoo.com>
2014-11-01 12:52:26 -0400

    Basic import scaffolding.

--- modules/manager/public/js/app/home.js
+++ modules/manager/public/js/app/home.js
@@ -1,30 +1,6 @@
-function Asset(data) {
-    this.id = data ? data.id : null;
-    this.fileName = data ? data.fileName : null;
-    this.createdAt = data ? data.createdAt : null;
-    this.updatedAt = data ? data.updatedAt : null;
-}
-
 HomeViewModel = BaseViewModel.extend({
     constructor : function() {
         this.base();
         console.log('Initializing home view model');
-
-        this.assets = ko.observableArray([]);
-
-        this.loadAssets();
-    },
-
-    loadAssets: function() {
-        var self = this;
-
-        var opts = {};
-        opts.url = "/api"
-        opts.successCallback = function(data, textStatus, jqXHR) {
-            //var assets = $.map(data.assets, function(asset) { return new Asset(asset) });
-            //self.assets(assets);
-        };
-
-        self.get(opts);
-    },
+    }
 });
---
+++ modules/manager/public/js/app/import.js
@@ -0,0 +1,29 @@
+function Asset(data) {
+    this.id = data ? data.id : null;
+    this.fileName = data ? data.fileName : null;
+    this.createdAt = data ? data.createdAt : null;
+    this.updatedAt = data ? data.updatedAt : null;
+}
+
+ImportViewModel = BaseViewModel.extend({
+    constructor : function() {
+        this.base();
+        console.log('Initializing import view model');
+
+        this.assets = ko.observableArray([]);
+        this.loadAssets();
+    },
+
+    loadAssets: function() {
+        var self = this;
+
+        var opts = {};
+        opts.url = "/api/import"
+        opts.successCallback = function(data, textStatus, jqXHR) {
+            //var assets = $.map(data.assets, function(asset) { return new Asset(asset) });
+            //self.assets(assets);
+        };
+
+        self.get(opts);
+    }
+});
