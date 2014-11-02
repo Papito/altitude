07413a9d9c48a0dca57389108f600a692797a27d
papito <a.taranchenko@yahoo.com>
2014-11-02 09:42:42 -0500

    Basic file traversal.

--- modules/common/build.sbt
+++ modules/common/build.sbt
@@ -2,5 +2,6 @@ Common.moduleSettings("common")
 
 libraryDependencies ++= Seq(
   "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test",
-  "org.json4s" %% "json4s-native" % "3.2.11"
+  "org.json4s" %% "json4s-native" % "3.2.11",
+  "org.apache.commons" % "commons-io" % "1.3.2"
 )
--- modules/manager/app/dao/manager/ImportDao.scala
+++ modules/manager/app/dao/manager/ImportDao.scala
@@ -1,15 +1,35 @@
 package dao.manager
 
 import play.api.Play
+import org.apache.commons.io.FileUtils
+import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions._
+import java.io.File
+
 import models.ImportAsset
 import util.log
 
 class ImportDao {
+  val imageSuffixes = List("jpg", "jpeg")
 
   def getImportAssets: List[ImportAsset] = {
-
-    val importPath = Play.current.configuration.getString("import.path").toString
+    val importPath = Play.current.configuration.getString("import.path").getOrElse("")
     log.info("Importing from '$importPath'", Map("importPath" -> importPath), log.STORAGE)
-    List[ImportAsset](new ImportAsset("file://path"))
+
+    val fileIterator = FileUtils.iterateFiles(new File(importPath), imageSuffixes.toArray, true)
+    val assets = new ListBuffer[ImportAsset]
+
+    for(file <- fileIterator) {
+      val f = new File(file.toString)
+      log.info("Processing $fileName", Map("fileName" -> file.toString), log.STORAGE)
+
+      val importAsset = new ImportAsset(
+        pathArg = f.getAbsolutePath
+      )
+      assets += importAsset
+    }
+
+    assets.toList
   }
+
 }
