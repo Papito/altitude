ee2713037dcb38947d437be29fef05478e0565d3
papito <a.taranchenko@yahoo.com>
2014-12-15 19:31:03 -0500

    More media type discovery tests.

--- modules/common/app/models/BaseModel.scala
+++ modules/common/app/models/BaseModel.scala
@@ -4,4 +4,5 @@ abstract class BaseModel(val id: String = null, val isClean: Boolean = false) {
 
   def toMap: Map[String, Any] = throw new NotImplementedError
   def fromMap(d: Map[String, Any]): BaseModel = throw new NotImplementedError
+  override def toString = toMap.toString()
 }
--- modules/manager/test/integration/ImportTests.scala
+++ modules/manager/test/integration/ImportTests.scala
@@ -9,22 +9,30 @@ import org.scalatest.Matchers._
 import org.scalatest.PartialFunctionValues._
 
 class ImportTests extends FunSuite with OneAppPerSuite {
-
   test("import file list") {
     val incomingPath = getClass.getResource("../files/incoming").getPath
     val assets = global.ManagerGlobal.importService.getAssetsToImport(path=incomingPath)
     assets should not be empty
   }
 
-  test("image media type") {
+  test("image media type (JPEG)") {
     val path = getClass.getResource("../files/incoming/images/1.jpg").getPath
     val importAsset = new ImportAsset(new File(path))
     val knownTypeImportAsset = global.ManagerGlobal.importService.getAssetWithType(importAsset)
 
-    // to map
     val d = knownTypeImportAsset.mediaType.toMap
     d.valueAt("type") should equal("image")
     d.valueAt("subtype") should equal("jpeg")
     d.valueAt("mime") should equal("image/jpeg")
+  }
+  test("audio media type (MP3)") {
+    val path = getClass.getResource("../files/incoming/audio/all.mp3").getPath
+    val importAsset = new ImportAsset(new File(path))
+    val knownTypeImportAsset = global.ManagerGlobal.importService.getAssetWithType(importAsset)
+
+    val d = knownTypeImportAsset.mediaType.toMap
+    d.valueAt("type") should equal("audio")
+    d.valueAt("subtype") should equal("mpeg")
+    d.valueAt("mime") should equal("audio/mpeg")
   }
 }
