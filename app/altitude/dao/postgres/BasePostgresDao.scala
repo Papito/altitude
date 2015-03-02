10d1a60b6d1d44fa80c759d114a84a7d6420e161
papito <a.taranchenko@yahoo.com>
2015-03-01 19:23:22 -0500

    Setup with Prequel JDBC wrapper.

--- app/altitude/dao/postgres/BasePostgresDao.scala
+++ app/altitude/dao/postgres/BasePostgresDao.scala
@@ -1,5 +1,7 @@
 package altitude.dao.postgres
 
+import play.api.db._
+
 import altitude.dao.BaseDao
 import altitude.util.log
 import play.api.Play
@@ -7,16 +9,19 @@ import play.api.libs.json.{JsValue, Json}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import play.api.Play.current
 
 abstract class BasePostgresDao(private val tableName: String) extends BaseDao {
   private val url = Play.current.configuration.getString("db.postgres.url").getOrElse("")
   require(url.nonEmpty)
   private val driver = Play.current.configuration.getString("db.postgres.driver").getOrElse("")
 
-  //def db = Database.forURL(url, driver = driver)
+  def datasource = DB.getDataSource("postgres")
+  //def db = Database.forURL()
 
   override def add(json: JsValue): Future[JsValue] = {
     log.info("POSTGRES INSERT")
+    datasource.getConnection
     Future[JsValue] {
       json
     }
