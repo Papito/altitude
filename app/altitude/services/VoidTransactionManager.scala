8dce6d333bb0c712d3e5c013d35ca01d3415480c
papito <a.taranchenko@yahoo.com>
2015-03-21 00:38:23 -0400

    Simplifying.

--- app/altitude/services/VoidTransactionManager.scala
+++ app/altitude/services/VoidTransactionManager.scala
@@ -1,25 +1,11 @@
 package altitude.services
 
-import altitude.util.log
-
 class VoidTransactionManager extends AbstractTransactionManager {
-  def transaction[A](f: => A) = {
-    log.info("TRANSACTION START")
-    val res = f
-    log.info("TRANSACTION END")
-    res
-  }
+  def transaction[A](f: => A) = f
 
-  def readOnly[A](f: => A) = {
-    log.info("READONLY")
-    f
-  }
+  def readOnly[A](f: => A) = f
 
-  def rollback() = {
-    log.info("ROLLBACK")
-  }
+  def rollback() = Unit
 
-  def commit() = {
-    log.info("COMMIT")
-  }
+  def commit() = Unit
 }
