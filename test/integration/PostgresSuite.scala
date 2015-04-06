07c82d22954b4ccf3e0da50e67ab363fed617adb
papito <a.taranchenko@yahoo.com>
2015-04-06 09:00:09 -0400

    Postgres suite resets the schema before all tests run (in case previous failure leaved the DB dirty).

--- test/integration/PostgresSuite.scala
+++ test/integration/PostgresSuite.scala
@@ -1,3 +1,25 @@
 package integration
 
-class PostgresSuite extends AllTests(Map("datasource" -> "postgres"))
+import altitude.dao.JdbcTransaction
+import org.scalatest.BeforeAndAfterAll
+import play.api.test.FakeApplication
+
+class PostgresSuite extends AllTests(config = Map("datasource" -> "postgres")) with BeforeAndAfterAll {
+
+  override def beforeAll(): Unit = {
+    /*
+    Reset the database schema once and run the migrations
+     */
+    val app = FakeApplication(additionalConfiguration = Map("datasource" -> "postgres"))
+    play.api.Play.start(app)
+
+    val tx = new JdbcTransaction
+    try {
+      val stmt = tx.conn.createStatement()
+      stmt.executeUpdate("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC")
+    } finally {
+      tx.close()
+      play.api.Play.stop()
+    }
+  }
+}
