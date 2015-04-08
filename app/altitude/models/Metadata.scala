cfc2712b0d65e8ebd2aa8a941e37cb1789677de9
papito <a.taranchenko@yahoo.com>
2015-04-08 04:48:35 -0400

    Fixing problems converting Asset from Json when Metadata is null

--- app/altitude/models/Metadata.scala
+++ app/altitude/models/Metadata.scala
@@ -1,24 +1,27 @@
 package altitude.models
 
 import altitude.exceptions.FormatException
-import play.api.libs.json.{JsResultException, JsValue, Json}
+import play.api.libs.json.{JsNull, JsResultException, JsValue, Json}
 import altitude.{Const => C}
 
 import scala.collection.immutable.HashMap
 import scala.language.implicitConversions
 
 object Metadata {
-  implicit def fromJson(json: JsValue): Metadata = try {
-    new Metadata(
-      id = (json \ C.Metadata.ID).as[String]
-    )} catch {
-    case e: JsResultException => throw new FormatException(s"Cannot convert from $json: ${e.getMessage}")
+  implicit def fromJson(json: JsValue): Metadata = json match {
+    case JsNull => new Metadata
+    case _ => try {
+      new Metadata(
+        id = (json \ C.Metadata.ID).as[String]
+      )} catch {
+      case e: JsResultException => throw new FormatException(s"Cannot convert from $json: ${e.getMessage}")
+    }
   }
 }
 
 case class Metadata(
-  override final val id: String = BaseModel.genId,
-  private val raw: Map[String, String] = new HashMap[String, String]())
+                     override final val id: String = BaseModel.genId,
+                     private val raw: Map[String, String] = new HashMap[String, String]())
   extends BaseModel(id) {
   override def toJson = Json.obj(
     C.Metadata.ID -> id
