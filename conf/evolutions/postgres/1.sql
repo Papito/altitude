8ba8f3992c9f74742d31ca350347b4874e360b6e
papito <a.taranchenko@yahoo.com>
2015-04-11 20:26:37 -0400

    Cleaning up the schema.

--- conf/evolutions/postgres/1.sql
+++ conf/evolutions/postgres/1.sql
@@ -19,40 +19,7 @@ CREATE TABLE asset (
     PRIMARY KEY (id)
 ) INHERITS (_core);
 
-CREATE OR REPLACE FUNCTION "update_json"(
-    "json" json,
-    "k"    TEXT,
-    "v"    anyelement
-) RETURNS json
-LANGUAGE sql
-IMMUTABLE
-STRICT
-AS $function$
-SELECT CASE
-       WHEN ("json" -> "k") IS NULL THEN "json"
-       ELSE (SELECT concat('{', string_agg(to_json("key") || ':' || "value", ','), '}')
-             FROM (SELECT *
-                   FROM json_each("json")
-                   WHERE "key" <> "k"
-                   UNION ALL
-                   SELECT "k", to_json("v")) AS "fields")::json
-       END
-$function$;
+# --- !Downs
 
-
-CREATE TYPE STORAGE_TYPE AS ENUM (
-    'fs',
-    'amazon_s3',
-    'amazon_cloud_drvie',
-    'google_drive'
-);
-
-
-CREATE TABLE storage (
-    id SERIAL,
-    name VARCHAR(100) NOT NULL,
-    type STORAGE_TYPE NOT NULL,
-    description TEXT,
-    PRIMARY KEY (id)
-) INHERITS (_core);
-
+DROP TABLE asset;
+DROP TABLE _core;
