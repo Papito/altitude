3f46d9d93de3b159c3dba8c319700f66ab430518
papito <a.taranchenko@yahoo.com>
2015-04-12 09:50:27 -0400

    Blowing away cruft.

--- app/altitude/models/MetaValue.scala
+++
@@ -1,5 +0,0 @@
-package altitude.models
-
-class MetaValue[T](val value: T, private val metaField: MetaField[T]) {
-  metaField.checkValue(value)
-}
--- app/altitude/models/TrueFalseMetaField.scala
+++
@@ -1,3 +0,0 @@
-package altitude.models
-
-class TrueFalseMetaField(name: String) extends MetaField[Boolean](name, isMulti=false)
