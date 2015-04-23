33817259d0d2ede2f9846e20add047c51854b30e
papito <a.taranchenko@yahoo.com>
2015-04-23 09:10:44 -0400

    Simplifying import tests.

--- test/integration/ImportTests.scala
+++ test/integration/ImportTests.scala
@@ -11,41 +11,21 @@ import scala.concurrent.Future
 
 @DoNotDiscover class ImportTests(val config: Map[String, _]) extends IntegrationTestCore {
   test("import image (JPEG)") {
-    val path = getClass.getResource("../files/incoming/images/1.jpg").getPath
-    val fileImportAsset = new FileImportAsset(new File(path))
-
-    val importedAsset = altitude.service.fileImport.importAsset(fileImportAsset).futureValue
-    val asset = altitude.service.library.getById(importedAsset.id.get).futureValue.get: Asset
-    asset.mediaType should equal(importedAsset.mediaType)
+    val asset = importFile("images/1.jpg")
   }
 
   test("import audio (MP3)") {
-    val path = getClass.getResource("../files/incoming/audio/all.mp3").getPath
+    val asset = importFile("audio/all.mp3")
+    (asset.metadata \ "Author").as[String] should equal("Whitney Houston")
+  }
+
+  protected def importFile(p: String): Asset = {
+    val path = getClass.getResource(s"../files/incoming/$p").getPath
     val fileImportAsset = new FileImportAsset(new File(path))
     val importedAsset = altitude.service.fileImport.importAsset(fileImportAsset).futureValue
     val asset = altitude.service.library.getById(importedAsset.id.get).futureValue.get: Asset
     asset.mediaType should equal(importedAsset.mediaType)
-    (asset.metadata \ "Author").as[String] should equal("Whitney Houston")
-  }
-
-  test("detect image media type (JPEG)") {
-    val path = getClass.getResource("../files/incoming/images/1.jpg").getPath
-    val fileImportAsset = new FileImportAsset(new File(path))
-    val assetType = altitude.service.fileImport.detectAssetType(fileImportAsset)
-
-    assetType.mediaType should equal ("image")
-    assetType.mediaSubtype should equal ("jpeg")
-    assetType.mime should equal ("image/jpeg")
-  }
-
-  test("detect audio media type (MP3)") {
-    val path = getClass.getResource("../files/incoming/audio/all.mp3").getPath
-    val fileImportAsset = new FileImportAsset(new File(path))
-    val assetType = altitude.service.fileImport.detectAssetType(fileImportAsset)
-
-    assetType.mediaType should equal ("audio")
-    assetType.mediaSubtype should equal ("mpeg")
-    assetType.mime should equal ("audio/mpeg")
+    asset
   }
 
   test("import file list") {
