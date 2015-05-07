11baf846211fb3aa1eb791f12783db6c6db6b117
papito <a.taranchenko@yahoo.com>
2015-05-07 09:05:35 -0400

    Generated Scalatra project.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Altitude #
+
+## Build & Run ##
+
+```sh
+$ cd Altitude
+$ ./sbt
+> container:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/altitude/AltitudeServlet.scala
@@ -0,0 +1,17 @@
+package altitude
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class AltitudeServlet extends AltitudeStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
