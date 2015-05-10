a9a0ca3a380f0a4a1e4aa0c1cbcc4954633cb3d0
papito <a.taranchenko@yahoo.com>
2015-05-10 19:08:03 -0400

    Adding Jade

--- src/main/scala/altitude/AltitudeServlet.scala
+++ src/main/scala/altitude/AltitudeServlet.scala
@@ -3,12 +3,8 @@ package altitude
 class AltitudeServlet extends AltitudeStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType="text/html"
+    jade("/hello-scalate")
   }
 
 }
