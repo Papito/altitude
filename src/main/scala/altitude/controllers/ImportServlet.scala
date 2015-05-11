a3604b65bf327a14cc102b8d51ce76f355af2122
papito <a.taranchenko@yahoo.com>
2015-05-10 20:06:40 -0400

    Logback config.

--- src/main/scala/altitude/controllers/ImportServlet.scala
+++ src/main/scala/altitude/controllers/ImportServlet.scala
@@ -1,6 +1,9 @@
 package altitude.controllers
 
+import org.slf4j.LoggerFactory
+
 class ImportServlet extends AltitudeStack {
+  val log =  LoggerFactory.getLogger(getClass)
 
   get("/") {
     contentType="text/html"
