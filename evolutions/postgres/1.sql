eeb4678c8f01bcf77b3f86dae239f01b583e9ad6
papito <a.taranchenko@yahoo.com>
2015-05-31 18:01:12 -0400

    Basic schema solution for tests. All pass for postgres.

---
+++ evolutions/postgres/1.sql
@@ -0,0 +1,21 @@
+DROP SCHEMA IF EXISTS "altitude-test" CASCADE; CREATE SCHEMA "altitude-test";
+
+CREATE TABLE _core (
+  created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+  updated_at timestamp WITHOUT TIME ZONE DEFAULT NULL
+);
+
+CREATE TABLE asset (
+  id varchar(24) PRIMARY KEY,
+  md5 varchar(32) NOT NULL,
+  media_type varchar(64) NOT NULL,
+  media_subtype varchar(64) NOT NULL,
+  mime_type varchar(64) NOT NULL,
+  metadata jsonb,
+  path TEXT NOT NULL,
+  filename TEXT NOT NULL,
+  image_data BYTEA,
+  size_bytes BIGINT NOT NULL
+) INHERITS (_core);
+
+CREATE UNIQUE INDEX asset_md5 ON asset(md5);
--- src/test/scala/integration/PostgresSuite.scala
+++ src/test/scala/integration/PostgresSuite.scala
@@ -1,32 +1,34 @@
 package integration
 
+import java.io.File
+import scala.io.Source._
+
 import altitude.{Altitude, Environment}
 import altitude.transactions.{TransactionId, Transaction, JdbcTransactionManager, JdbcTransaction}
+import org.apache.commons.io.FileUtils
 import org.scalatest.BeforeAndAfterAll
 import org.slf4j.LoggerFactory
 
-class PostgresSuite extends AllTests(config = Map("datasource" -> "postgres")) with BeforeAndAfterAll {
+class PostgresSuite extends AllTests(config = Map("datasource" -> "postgres"))
+  with BeforeAndAfterAll {
   Environment.ENV = Environment.TEST
   val log =  LoggerFactory.getLogger(getClass)
 
   override def beforeAll(): Unit = {
     //Reset the database schema once
+
     val altitude: Altitude = new Altitude
     implicit val txId: TransactionId = new TransactionId
-
     val txManager = new JdbcTransactionManager(altitude)
+
+    val evolutionPath = new java.io.File( "evolutions/postgres/1.sql" ).getCanonicalPath
+    val sql = scala.io.Source.fromFile(evolutionPath).mkString
+    log.info(s"Running $sql")
 
     txManager.withTransaction {
       log.info("SETUP")
       val stmt = txManager.transaction.conn.createStatement()
-      stmt.executeUpdate("DROP SCHEMA IF EXISTS \"altitude-test\" CASCADE; CREATE SCHEMA \"altitude-test\";")
-      stmt.executeUpdate("""
-                           |DROP TABLE IF EXISTS test;
-                           |CREATE TABLE test (
-                           | id varchar(24) NOT NULL,
-                           | created_at TIMESTAMP,
-                           | updated_at TIMESTAMP)
-                         """.stripMargin)
+      stmt.executeUpdate(sql)
     }
     /*
       We have to commit this, however, later we make sure everything is rolled back.
