01adaa809807f1dbf6bc6c264170002e00e836c5
papito <a.taranchenko@yahoo.com>
2015-06-06 17:20:28 -0400

    Updating dependency versions and including the ones needed for websockets.

--- project/build.scala
+++ project/build.scala
@@ -21,18 +21,27 @@ object AltitudeBuild extends Build {
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
       resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
+      resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+      resolvers += "Akka Repo" at "http://repo.akka.io/repository",
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
+        "org.scalatra" %% "scalatra-atmosphere" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-json" % ScalatraVersion,
 
-        "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
         "org.scalatra" %% "scalatra-scalatest" % "2.4.0.RC1" % "test",
+        "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
+
+        "org.json4s" %% "json4s-native" % "3.2.11",
+        "org.json4s" %% "json4s-jackson" % "3.2.10",
+        "org.json4s" %% "json4s-mongo" % "3.2.10",
+
         "org.apache.tika" % "tika-parsers" % "1.7",
         "org.apache.tika" % "tika-serialization" % "1.7",
-        "org.json4s" %% "json4s-native" % "3.2.11",
         "org.apache.commons" % "commons-io" % "1.3.2",
+
+        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
         "net.codingwell" %% "scala-guice" % "4.0.0-beta5",
         "commons-dbutils" % "commons-dbutils" % "1.6",
         "org.imgscalr" % "imgscalr-lib" % "4.2",
@@ -40,11 +49,10 @@ object AltitudeBuild extends Build {
         "com.typesafe.play" %% "play-json" % "2.3.4",
         "org.mongodb" % "casbah_2.11" % "2.8.1",
         "org.mongodb" % "casbah-commons_2.11" % "2.8.1",
-        "org.json4s" %% "json4s-jackson" % "3.2.10",
-        "org.json4s" %% "json4s-mongo" % "3.2.10",
 
-        "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "container",
-        "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "container",
+        "org.eclipse.jetty" % "jetty-websocket" % "8.1.17.v20150415" % "container",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.2.11.v20150529" % "container",
+        "org.eclipse.jetty" % "jetty-plus" % "9.2.11.v20150529" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
