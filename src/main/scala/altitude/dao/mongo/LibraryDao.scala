696fbc83d55d61cca9091244d36c7cc80274ef49
papito <a.taranchenko@yahoo.com>
2015-07-15 16:45:35 -0400

    FIXME

--- src/main/scala/altitude/dao/mongo/LibraryDao.scala
+++ src/main/scala/altitude/dao/mongo/LibraryDao.scala
@@ -15,45 +15,50 @@ class LibraryDao(val app: Altitude) extends BaseMongoDao("assets") with altitude
   protected def GRID_FS = GridFS(DB, "preview")
 
   override def addPreview(asset: Asset, bytes: Array[Byte])(implicit txId: TransactionId = new TransactionId): Option[String] = {
-    bytes.length > 0 match {
-      case false => None
-      case true =>
-        log.info(s"Saving image preview for ${asset.path}")
-        val is: InputStream = new ByteArrayInputStream(bytes)
-        val id = GRID_FS(is) { fh =>
-          fh.filename = asset.path
-          fh.contentType = asset.mediaType.mime
-        }
-        //FIXME: Option in a try/catch
+    if (bytes.length < 0) return None
 
-        is.close()
+    log.info(s"Saving image preview for ${asset.path}")
 
-        Some(Base64.encodeBase64String(bytes))
+    var is: Option[InputStream] = None
+
+    try {
+      is = Some(new ByteArrayInputStream(bytes))
+      GRID_FS(is.get) { fh =>
+        fh.filename = asset.path
+        fh.contentType = asset.mediaType.mime
+      }
     }
+    finally {
+      if (is.isDefined) is.get.close()
+    }
+
+    Some(Base64.encodeBase64String(bytes))
   }
 
   override def getPreview(id: String)(implicit txId: TransactionId = new TransactionId): Option[Preview] = {
     log.debug(s"Getting preview for '$id'")
-
     // get the asset
     val assetJson: Option[JsObject] = this.getById(id)
     //FIXME: assuming the asset exists, but NotFound should be thrown earlier
     val asset: Asset = Asset.fromJson(assetJson.get)
     val gridFsFile: Option[GridFSDBFile] = GRID_FS.findOne(asset.path)
 
-    gridFsFile.isDefined match {
-      case false => None
-      case true =>
-        val is: InputStream = gridFsFile.get.inputStream
-        val bytes: Array[Byte] = IOUtils.toByteArray(is);
-        val preview: Preview = Preview(
-          id = Some(gridFsFile.get.id.toString),
-          data = bytes,
-          mime = asset.mediaType.mime)
-        is.close()
-        //FIXME: Option in a try/catch
+    if (gridFsFile.isEmpty)
+      return None
 
-        Some(preview)
+    var is: Option[InputStream] = None
+
+    try {
+      is = Some(gridFsFile.get.inputStream)
+      val bytes: Array[Byte] = IOUtils.toByteArray(is.get)
+      val preview: Preview = Preview(
+        id = Some(gridFsFile.get.id.toString),
+        data = bytes,
+        mime = asset.mediaType.mime)
+      Some(preview)
+    }
+    finally {
+      if (is.isDefined) is.get.close()
     }
   }
 }
