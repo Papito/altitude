37e9794b88ca831ec573ea3cff27d41c3dc0f37d
papito <a.taranchenko@yahoo.com>
2015-09-04 09:04:18 -0400

    Gracefully handling dead preview images.

--- src/main/scala/altitude/controllers/AssetServlet.scala
+++ src/main/scala/altitude/controllers/AssetServlet.scala
@@ -1,12 +1,19 @@
 package altitude.controllers
 
+import altitude.exceptions.NotFoundException
 import altitude.models.Preview
 
 class AssetServlet extends BaseController {
   get("/:id/preview") {
     val id = params("id")
-    val preview: Preview = this.app.service.library.getPreview(id)
-    this.contentType = preview.mime_type
-    preview.data
+
+    try {
+      val preview: Preview = this.app.service.library.getPreview(id)
+      this.contentType = preview.mime_type
+      preview.data
+    }
+    catch {
+      case ex: NotFoundException => redirect("/i/1x1.png")
+    }
   }
 }
