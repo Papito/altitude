df4eb8bd2abd78f01b73066f83d3b16661efb961
papito <a.taranchenko@yahoo.com>
2015-09-10 18:54:13 -0400

    Rest of sanitizers.

--- src/main/scala/altitude/Cleaners.scala
+++ src/main/scala/altitude/Cleaners.scala
@@ -5,11 +5,10 @@ import play.api.libs.json.{JsObject, Json}
 object Cleaners {
   case class Cleaner(trim: Option[List[String]] = None,
                      lower:  Option[List[String]] = None,
-                     defaults:  Option[Map[String, Short]] = None,
-                     maxLenghts:  Option[Map[String, Short]] = None) {
+                     defaults:  Option[Map[String, String]] = None) {
 
     def clean(json: JsObject): JsObject = {
-      doLower(maxLength(doDefaults(doTrim(json))))
+      doLower(doDefaults(doTrim(json)))
     }
 
     protected def doTrim(json: JsObject): JsObject = {
@@ -22,18 +21,22 @@ object Cleaners {
     }
 
     protected def doLower(json: JsObject): JsObject = {
-      if (lower.isDefined) return json
-      json
+      lower.getOrElse(List[String]()).fold(json) { (res, field) =>
+        (json \ field.toString).asOpt[String] match {
+          case v: Some[String] if v.nonEmpty => json ++ Json.obj(field.toString -> v.get.toLowerCase)
+          case _ => json
+        }
+      }.asInstanceOf[JsObject]
     }
 
     protected def doDefaults(json: JsObject): JsObject = {
-      if (defaults.isDefined) return json
-      json
+      defaults.getOrElse(Map[String, String]()).keys.fold(json) { (res, field) =>
+        (json \ field.toString).asOpt[String] match {
+          case v: Some[String] if v.isEmpty => json ++ Json.obj(
+            field.toString -> defaults.get(field.toString) )
+          case _ => json
+        }
+      }.asInstanceOf[JsObject]
     }
-    protected def maxLength(obj: JsObject): JsObject = {
-      if (maxLenghts.isDefined) return obj
-      obj
-    }
-
   }
 }
