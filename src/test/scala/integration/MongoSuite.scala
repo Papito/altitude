c4d41bfa965b0408910a41959f5a3327063a644e
papito <a.taranchenko@yahoo.com>
2015-09-14 19:55:06 -0400

    Deleting test mongo databases.

--- src/test/scala/integration/MongoSuite.scala
+++ src/test/scala/integration/MongoSuite.scala
@@ -1,3 +1,17 @@
 package integration
 
-class MongoSuite extends AllTests(Map("datasource" -> "mongo"))
+import altitude.Altitude
+import altitude.dao.mongo.BaseMongoDao
+import org.scalatest.BeforeAndAfterAll
+
+class MongoSuite extends AllTests(config = Map("datasource" -> "mongo")) with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    // delete all test databases created in this suite
+    val altitude: Altitude = new Altitude(additionalConfiguration = config)
+    val client = BaseMongoDao.client(altitude)
+    client.databaseNames().filter(_.startsWith("altitude-test-")).foreach {dbName: String =>
+        client.dropDatabase(dbName)
+    }
+  }
+}
