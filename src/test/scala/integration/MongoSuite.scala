eae7999866bc887a7b524f71fee30213c2c4edcc
papito <a.taranchenko@yahoo.com>
2015-09-14 21:34:22 -0400

    Deleting all test datases but avoiding the current client.

--- src/test/scala/integration/MongoSuite.scala
+++ src/test/scala/integration/MongoSuite.scala
@@ -10,7 +10,12 @@ class MongoSuite extends AllTests(config = Map("datasource" -> "mongo")) with Be
     // delete all test databases created in this suite
     val altitude: Altitude = new Altitude(additionalConfiguration = config)
     val client = BaseMongoDao.client(altitude)
-    client.databaseNames().filter(_.startsWith("altitude-test-")).foreach {dbName: String =>
+    val thisDbName = altitude.config.getString("db.mongo.db")
+
+    client.databaseNames().
+      filter({_.startsWith("altitude-test-")}).
+      filter(_ != thisDbName).foreach {dbName: String =>
+        println(s"Deleting $dbName")
         client.dropDatabase(dbName)
     }
   }
