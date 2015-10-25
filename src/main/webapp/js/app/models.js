38c31d33570e8b3420de77eb0d514445f62dcafa
papito <Andrei Taranchenko drey10@gmail.com>
2015-10-25 04:17:49 -0400

    Reformat

--- src/main/webapp/js/app/models.js
+++ src/main/webapp/js/app/models.js
@@ -1,7 +1,7 @@
 function Asset(data) {
-    this.id = data ? data.id : null;
-    this.path = data? data.path: null;
-    this.fileName = data ? data.fileName : null;
-    this.createdAt = data ? data.createdAt : null;
-    this.updatedAt = data ? data.updatedAt : null;
+  this.id = data ? data.id : null;
+  this.path = data ? data.path : null;
+  this.fileName = data ? data.fileName : null;
+  this.createdAt = data ? data.createdAt : null;
+  this.updatedAt = data ? data.updatedAt : null;
 }
