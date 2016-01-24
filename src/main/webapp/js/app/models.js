53a42b446d9107f9eaf483322bafd34be8c09fd4
papito <Andrei Taranchenko drey10@gmail.com>
2016-01-24 06:57:06 -0500

    Asset counts for non-system folders.

--- src/main/webapp/js/app/models.js
+++ src/main/webapp/js/app/models.js
@@ -2,8 +2,6 @@ function Asset(data) {
   this.id = data ? data.id : null;
   this.path = data ? data.path : null;
   this.fileName = data ? data.filename : null;
-  this.createdAt = data ? data.created_at : null;
-  this.updatedAt = data ? data.updated_at : null;
 }
 
 function Folder(data) {
