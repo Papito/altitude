9ee8039a3587ea72bd1f9f961872893bb2339a6a
papito <Andrei Taranchenko drey10@gmail.com>
2016-06-12 18:36:38 -0400

    Incrementing stats but still wrong.

--- src/main/scala/altitude/service/AssetService.scala
+++ src/main/scala/altitude/service/AssetService.scala
@@ -2,11 +2,21 @@ package altitude.service
 
 import altitude.Altitude
 import altitude.dao.AssetDao
-import altitude.models.Asset
+import altitude.models.{Stats, Asset}
+import altitude.models.search.Query
+import altitude.transactions.TransactionId
 import net.codingwell.scalaguice.InjectorExtensions._
 import org.slf4j.LoggerFactory
+import play.api.libs.json.JsObject
 
 class AssetService(app: Altitude) extends BaseService[Asset](app) {
   private final val log = LoggerFactory.getLogger(getClass)
   override protected val DAO = app.injector.instance[AssetDao]
+
+  override def add(asset: Asset, queryForDup: Option[Query] = None)(implicit txId: TransactionId = new TransactionId): JsObject = {
+    txManager.withTransaction[JsObject] {
+      app.service.stats.incrementStat(Stats.TOTAL_ASSETS)
+      super.add(asset, queryForDup)
+    }
+  }
 }
