f2916e6a4b00a79272fa1d00cf05827c8d1009b9
papito <Andrei Taranchenko drey10@gmail.com>
2016-06-13 06:19:50 -0400

    Updating trash test.

--- src/test/scala/integration/TrashServiceTests.scala
+++ src/test/scala/integration/TrashServiceTests.scala
@@ -9,7 +9,7 @@ import org.scalatest.DoNotDiscover
   test("move to trash") {
     val asset: Asset = altitude.service.library.add(makeAsset(Folder.UNCATEGORIZED))
     altitude.service.asset.getAll.length should be (1)
-    altitude.service.trash.recycleAsset(asset.id.get)
+    altitude.service.library.recycleAsset(asset.id.get)
     altitude.service.asset.getAll.length should be (0)
     altitude.service.trash.getAll.length should be (1)
   }
