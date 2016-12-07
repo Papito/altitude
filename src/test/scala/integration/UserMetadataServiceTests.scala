3a3cfe8d5d0c3c5584cdc8206f1f18189b896c46
papito <Andrei Taranchenko drey10@gmail.com>
2016-12-07 05:10:06 -0500

    Related to previous commit - updating test names.

--- src/test/scala/integration/UserMetadataServiceTests.scala
+++ src/test/scala/integration/UserMetadataServiceTests.scala
@@ -7,7 +7,7 @@ import org.scalatest.Matchers._
 
 @DoNotDiscover class UserMetadataServiceTests(val config: Map[String, String]) extends IntegrationTestCore {
 
-  test("add/get user fixed list field") {
+  test("add/get user constraint list") {
     val metadataField = altitude.service.userMetadata.addField(
       UserMetadataField(
         userId = CURRENT_USER_ID,
@@ -29,7 +29,7 @@ import org.scalatest.Matchers._
     storedField.constraintList.get.length should be(metadataField.constraintList.get.length)
   }
 
-  test("add/get user metadata fields") {
+  test("add/get user fields") {
     SET_USER_1()
     val metadataField = altitude.service.userMetadata.addField(
       UserMetadataField(userId = CURRENT_USER_ID, name = "field name", fieldType = FieldType.STRING.toString))
@@ -44,7 +44,7 @@ import org.scalatest.Matchers._
     altitude.service.userMetadata.getFieldById(metadataField.id.get) should not be None
   }
 
-  test("get all user metadata fields") {
+  test("get all user fields") {
     altitude.service.userMetadata.addField(
       UserMetadataField(userId = CURRENT_USER_ID, name = "field name 1", fieldType = FieldType.STRING.toString))
     altitude.service.userMetadata.addField(
@@ -61,7 +61,7 @@ import org.scalatest.Matchers._
     altitude.service.userMetadata.getAllFields.length should be(1)
   }
 
-  test("delete user metadata field") {
+  test("delete user field") {
     val metadataField = altitude.service.userMetadata.addField(
       UserMetadataField(userId = CURRENT_USER_ID, name = "fieldName", fieldType = FieldType.FLAG.toString))
 
