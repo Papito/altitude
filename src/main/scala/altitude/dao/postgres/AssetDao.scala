9dd0cc468b737fa836e1fd0f777578953495b66c
papito <Andrei Taranchenko drey10@gmail.com>
2017-01-04 07:50:03 -0500

    Adding JSON retrieval logic for Postgres.

--- src/main/scala/altitude/dao/postgres/AssetDao.scala
+++ src/main/scala/altitude/dao/postgres/AssetDao.scala
@@ -1,6 +1,26 @@
 package altitude.dao.postgres
 
-import altitude.Altitude
+import altitude.models.Metadata
+import altitude.transactions.TransactionId
+import altitude.{Const => C, Context, Altitude}
+import play.api.libs.json.{JsObject, Json}
 
+class AssetDao(app: Altitude) extends altitude.dao.jdbc.AssetDao(app) with Postgres {
 
-class AssetDao(app: Altitude) extends altitude.dao.jdbc.AssetDao(app) with Postgres
+  override def getMetadata(assetId: String)(implicit ctx: Context, txId: TransactionId): Option[Metadata] = {
+    val sql = s"""
+      SELECT (${C.Asset.METADATA}#>>'{}')::text as ${C.Asset.METADATA}
+         FROM $tableName
+       WHERE ${C.Base.REPO_ID} = ? AND ${C.Asset.ID} = ?
+      """
+
+    oneBySqlQuery(sql, List(ctx.repo.id.get, assetId)) match {
+      case Some(rec) =>
+        val metadataJson: String = rec.getOrElse(C.Asset.METADATA, "{}").asInstanceOf[String]
+        val json = Json.parse(metadataJson).as[JsObject]
+        val metadata = Metadata.fromJson(json)
+        Some(metadata)
+      case None => None
+    }
+  }
+}
