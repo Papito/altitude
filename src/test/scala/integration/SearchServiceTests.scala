7d7306fb396f593f31e693be0d2ef2a148febb24
papito <Andrei Taranchenko drey10@gmail.com>
2017-01-11 17:13:43 -0500

    Some tests.

--- src/test/scala/integration/SearchServiceTests.scala
+++ src/test/scala/integration/SearchServiceTests.scala
@@ -16,10 +16,23 @@ import org.scalatest.DoNotDiscover
         name = "field 2",
         fieldType = FieldType.NUMBER))
 
-    val data = Map[String, Set[String]](
+    val field3 = altitude.service.metadata.addField(
+      MetadataField(
+        name = "field 3",
+        fieldType = FieldType.BOOL))
+
+    var data = Map[String, Set[String]](
         field1.id.get -> Set("one", "two", "three"),
-        field2.id.get -> Set("1", "2", "3.002", "14.1", "1.25", "123456789"))
+        field2.id.get -> Set("1", "2", "3.002", "14.1", "1.25", "123456789"),
+        field3.id.get -> Set("true"))
     val assetData1 = makeAsset(altitude.service.folder.getUncatFolder, new Metadata(data))
     val asset1: Asset = altitude.service.library.add(assetData1)
+
+    data = Map[String, Set[String]](
+        field1.id.get -> Set("six", "seven"),
+        field2.id.get -> Set("5", "1001", "1"),
+        field3.id.get -> Set("true"))
+    val assetData2 = makeAsset(altitude.service.folder.getUncatFolder, new Metadata(data))
+    val asset2: Asset = altitude.service.library.add(assetData2)
   }
 }
