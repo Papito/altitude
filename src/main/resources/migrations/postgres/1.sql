b1031c249d018611fd561f06fcca65a7e6e3d21d
papito <Andrei Taranchenko drey10@gmail.com>
2017-01-17 08:41:44 -0500

    Schema casing.

--- src/main/resources/migrations/postgres/1.sql
+++ src/main/resources/migrations/postgres/1.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS "altitude-test";
-
 CREATE TABLE _core (
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT NULL
@@ -13,34 +11,34 @@ CREATE UNIQUE INDEX system_record ON system(id);
 INSERT INTO system(id, version) VALUES(0, 0);
 
 CREATE TABLE repository(
-  id char(24) PRIMARY KEY,
-  name varchar(255) NOT NULL,
+  id CHAR(24) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
   description TEXT,
-  root_folder_id char(24) NOT NULL,
-  uncat_folder_id char(24) NOT NULL
+  root_folder_id CHAR(24) NOT NULL,
+  uncat_folder_id CHAR(24) NOT NULL
 ) INHERITS (_core);
 
 CREATE TABLE stats (
-  repository_id char(24) NOT NULL,
-  dimension varchar(60) PRIMARY KEY,
+  repository_id CHAR(24) NOT NULL,
+  dimension VARCHAR(60) PRIMARY KEY,
   dim_val INT NOT NULL DEFAULT 0
 );
 CREATE INDEX stats_01 ON stats(repository_id);
 CREATE UNIQUE INDEX stats_02 ON stats(repository_id, dimension);
 
 CREATE TABLE asset (
-  id char(24) PRIMARY KEY,
-  repository_id char(24) NOT NULL,
-  user_id char(24) NOT NULL,
-  md5 varchar(32) NOT NULL,
-  media_type varchar(64) NOT NULL,
-  media_subtype varchar(64) NOT NULL,
-  mime_type varchar(64) NOT NULL,
+  id CHAR(24) PRIMARY KEY,
+  repository_id CHAR(24) NOT NULL,
+  user_id CHAR(24) NOT NULL,
+  md5 VARCHAR(32) NOT NULL,
+  media_type VARCHAR(64) NOT NULL,
+  media_subtype VARCHAR(64) NOT NULL,
+  mime_type VARCHAR(64) NOT NULL,
   metadata jsonb,
   extracted_metadata jsonb,
   raw_metadata jsonb,
   path TEXT NOT NULL,
-  folder_id char(24) NOT NULL DEFAULT '1',
+  folder_id CHAR(24) NOT NULL DEFAULT '1',
   filename TEXT NOT NULL,
   size_bytes INT NOT NULL,
   is_recycled BOOLEAN NOT NULL DEFAULT FALSE
@@ -50,30 +48,30 @@ CREATE UNIQUE INDEX asset_02 ON asset(repository_id, path, is_recycled);
 CREATE INDEX asset_03 ON asset(repository_id, folder_id, is_recycled);
 
 CREATE TABLE metadata_field (
-  id char(24) PRIMARY KEY,
-  repository_id char(24) NOT NULL,
-  name varchar(255) NOT NULL,
-  name_lc varchar(255) NOT NULL,
-  field_type varchar(255) NOT NULL
+  id CHAR(24) PRIMARY KEY,
+  repository_id CHAR(24) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  name_lc VARCHAR(255) NOT NULL,
+  field_type VARCHAR(255) NOT NULL
 ) INHERITS (_core);
 CREATE INDEX metadata_field_01 ON metadata_field(repository_id);
 CREATE UNIQUE INDEX metadata_field_02 ON metadata_field(repository_id, name_lc);
 
 CREATE TABLE folder (
-  id char(24) PRIMARY KEY,
-  repository_id char(24) NOT NULL,
-  name varchar(255) NOT NULL,
-  name_lc varchar(255) NOT NULL,
-  parent_id char(24) NOT NULL,
+  id CHAR(24) PRIMARY KEY,
+  repository_id CHAR(24) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  name_lc VARCHAR(255) NOT NULL,
+  parent_id CHAR(24) NOT NULL,
   num_of_assets INTEGER NOT NULL DEFAULT 0
 ) INHERITS (_core);
 CREATE INDEX folder_01 ON folder(repository_id, parent_id);
 CREATE UNIQUE INDEX folder_02 ON folder(repository_id, parent_id, name_lc);
 
 CREATE TABLE search_parameter (
-  repository_id char(24) NOT NULL,
-  asset_id char(24) NOT NULL,
-  field_id char(24) NOT NULL,
+  repository_id CHAR(24) NOT NULL,
+  asset_id CHAR(24) NOT NULL,
+  field_id CHAR(24) NOT NULL,
   field_value_txt TEXT NOT NULL,
   field_value_kw TEXT NULL,
   field_value_num DECIMAL,
@@ -88,8 +86,8 @@ CREATE INDEX search_parameter_05 ON search_parameter(repository_id, field_id, fi
 CREATE INDEX search_parameter_06 ON search_parameter(repository_id, field_id, field_value_dt);
 
 CREATE TABLE search_document (
-  repository_id char(24) NOT NULL,
-  asset_id char(24) NOT NULL,
+  repository_id CHAR(24) NOT NULL,
+  asset_id CHAR(24) NOT NULL,
   path TEXT NOT NULL,
   metadata_values TEXT,
   extracted_metadata_values TEXT,
--- src/main/resources/migrations/sqlite/1.sql
+++ src/main/resources/migrations/sqlite/1.sql
@@ -6,38 +6,39 @@ CREATE UNIQUE INDEX system_01 ON system(id);
 INSERT INTO system(id, version) VALUES(0, 0);
 
 CREATE TABLE repository(
-  id char(24) PRIMARY KEY,
-  name varchar(255) NOT NULL,
+  id CHAR(24) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
   description TEXT,
-  root_folder_id char(24) NOT NULL,
-  uncat_folder_id char(24) NOT NULL,
+  root_folder_id CHAR(24) NOT NULL,
+  uncat_folder_id CHAR(24) NOT NULL,
   created_at DATE DEFAULT (datetime('now', 'utc')),
   updated_at DATE DEFAULT NULL
 );
 
 CREATE TABLE stats (
-  repository_id char(24) NOT NULL,
-  dimension varchar(60) PRIMARY KEY,
+  repository_id CHAR(24) NOT NULL,
+  dimension VARCHAR(60) PRIMARY KEY,
   dim_val INT NOT NULL DEFAULT 0
 );
 CREATE INDEX stats_01 ON stats(repository_id);
 CREATE UNIQUE INDEX stats_02 ON stats(repository_id, dimension);
 
 CREATE TABLE asset  (
-  id char(24) PRIMARY KEY,
-  repository_id char(24) NOT NULL,
-  user_id char(24) NOT NULL,
-  md5 varchar(32) NOT NULL,
-  media_type varchar(64) NOT NULL,
-  media_subtype varchar(64) NOT NULL,
-  mime_type varchar(64) NOT NULL,
+  id CHAR(24) PRIMARY KEY,
+  repository_id CHAR(24) NOT NULL,
+  user_id CHAR(24) NOT NULL,
+  md5 VARCHAR(32) NOT NULL,
+  media_type VARCHAR(64) NOT NULL,
+  media_subtype VARCHAR(64) NOT NULL,
+  mime_type VARCHAR(64) NOT NULL,
   extracted_metadata TEXT,
   raw_metadata TEXT,
   metadata TEXT,
   path TEXT NOT NULL,
-  folder_id char(24) NOT NULL,
+  folder_id CHAR(24) NOT NULL,
   filename TEXT NOT NULL,
   size_bytes INT NOT NULL,
+  is_recycled TINYINT NOT NULL DEFAULT FALSE,
   created_at DATE DEFAULT (datetime('now', 'utc')),
   updated_at DATE DEFAULT NULL
 );
@@ -46,11 +47,11 @@ CREATE UNIQUE INDEX asset_02 ON asset(repository_id, path);
 CREATE INDEX asset_03 ON asset(repository_id, folder_id);
 
 CREATE TABLE metadata_field (
-  id char(24) PRIMARY KEY,
-  repository_id char(24) NOT NULL,
-  name varchar(255) NOT NULL,
-  name_lc varchar(255) NOT NULL,
-  field_type varchar(255) NOT NULL,
+  id CHAR(24) PRIMARY KEY,
+  repository_id CHAR(24) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  name_lc VARCHAR(255) NOT NULL,
+  field_type VARCHAR(255) NOT NULL,
   created_at DATE DEFAULT (datetime('now', 'utc')),
   updated_at DATE DEFAULT NULL
 );
@@ -59,11 +60,11 @@ CREATE UNIQUE INDEX metadata_field_02 ON metadata_field(repository_id, name_lc);
 
 
 CREATE TABLE folder (
-  id char(24) PRIMARY KEY,
-  repository_id char(24) NOT NULL,
-  name varchar(255) NOT NULL,
-  name_lc varchar(255) NOT NULL,
-  parent_id char(24) NOT NULL,
+  id CHAR(24) PRIMARY KEY,
+  repository_id CHAR(24) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  name_lc VARCHAR(255) NOT NULL,
+  parent_id CHAR(24) NOT NULL,
   num_of_assets INTEGER NOT NULL DEFAULT 0,
   created_at DATE DEFAULT (datetime('now', 'utc')),
   updated_at DATE DEFAULT NULL
@@ -72,9 +73,9 @@ CREATE INDEX folder_01 ON folder(repository_id, parent_id);
 CREATE UNIQUE INDEX folder_02 ON folder(repository_id, parent_id, name_lc);
 
 CREATE TABLE search_parameter (
-  repository_id char(24) NOT NULL,
-  asset_id char(24) NOT NULL,
-  field_id char(24) NOT NULL,
+  repository_id CHAR(24) NOT NULL,
+  asset_id CHAR(24) NOT NULL,
+  field_id CHAR(24) NOT NULL,
   field_value_txt TEXT NOT NULL,
   field_value_kw TEXT NULL,
   field_value_num DECIMAL,
