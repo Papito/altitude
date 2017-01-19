c57f2f4bd91735bab56642a526584746c877da0d
papito <Andrei Taranchenko drey10@gmail.com>
2017-01-19 04:04:06 -0500

    Query object stuff.

--- src/main/scala/altitude/util/Query.scala
+++ src/main/scala/altitude/util/Query.scala
@@ -2,10 +2,10 @@ package altitude.util
 
 object Query {
   object ParamType extends Enumeration {
-    val EQ, NOT_EQ, GT, LT, GTE, LTE, IN, RANGE, OR, CONTAINS, MATCHES = Value
+    val EQ, GT, LT, GTE, LTE, IN, RANGE, OR, CONTAINS, MATCHES = Value
   }
 
-  case class QueryParam private (values: Set[Any], paramType: ParamType.Value) {
+  case class QueryParam private (values: Set[Any], paramType: ParamType.Value, negate: Boolean = false) {
     require(values.nonEmpty)
 
     // a range requires two values
@@ -13,42 +13,54 @@ object Query {
       require(values.size == 2)
 
     // overloaded to accept one value
-    def this(value: Any, paramType: ParamType.Value) =
-      this(Set(value), paramType)
+    def this (value: Any, paramType: ParamType.Value, negate: Boolean) =
+      this(Set(value), paramType, negate)
 
     // overloaded to accept two values
-    def this(value1: Any, value2: Any, paramType: ParamType.Value) =
-      this(value1 :: value2 :: Nil, paramType)
+    def this(value1: Any, value2: Any, paramType: ParamType.Value, negate: Boolean) =
+      this(value1 :: value2 :: Nil, paramType, negate)
 
     // overloaded as simple equals
     def this(value: Any) =
-      this(value, ParamType.EQ)
+      this(value, ParamType.EQ, negate = false)
   }
 
   def EQUALS(value: Any) = new QueryParam(value)
-
-  def NOT_EQUALS(value: Any) = new QueryParam(value, ParamType.NOT_EQ)
+  def NOT_EQUALS(value: Any) = new QueryParam(value, ParamType.EQ, negate = true)
 
   def LT(value: Any) = new QueryParam(value, ParamType.LT)
-
-  def GT(value: Any) = new QueryParam(value, ParamType.GT)
+  def NOT_LT(value: Any) = new QueryParam(value, ParamType.LT, negate = true)
 
   def LTE(value: Any) = new QueryParam(value, ParamType.LTE)
+  def NOT_LTE(value: Any) = new QueryParam(value, ParamType.LTE, negate = true)
+
+  def GT(value: Any) = new QueryParam(value, ParamType.GT)
+  def NOT_GT(value: Any) = new QueryParam(value, ParamType.GT, negate = true)
 
   def GTE(value: Any) = new QueryParam(value, ParamType.GTE)
+  def NOT_GTE(value: Any) = new QueryParam(value, ParamType.GTE, negate = true)
 
   def RANGE(values: List[Any]) = new QueryParam(values, ParamType.RANGE)
+  def NOT_IN_RANGE(values: List[Any]) = new QueryParam(values, ParamType.RANGE, negate = true)
 
   def OR(values: List[Any]) = new QueryParam(values, ParamType.OR)
+  def NOT_OR(values: List[Any]) = new QueryParam(values, ParamType.OR, negate = true)
 
-  def IN(values: List[Any]) = {
+  def IN(values: List[Any], negate: Boolean = false) = {
     // if only one value given - simplify this to be just an equals
     if (values.size == 1) {
-      new QueryParam(values.head)
+      new QueryParam(values.head, negate)
     } else {
-      new QueryParam(values, ParamType.IN)
+      new QueryParam(values, ParamType.IN, negate)
     }
   }
+  def NOT_IN(values: List[Any]) = IN(values, negate = true)
+
+  def CONTAINS (value: Any) = new QueryParam(value, ParamType.CONTAINS)
+  def NOT_CONTAINS(value: Any) = new QueryParam(value, ParamType.CONTAINS, negate = true)
+
+  def MATCHES(value: Any) = new QueryParam(value, ParamType.MATCHES)
+  def NOT_MATCHES(value: Any) = new QueryParam(value, ParamType.MATCHES, negate = true)
 }
 
 case class Query(params: Map[String, Any] = Map(), rpp: Int = 0, page: Int = 1) {
