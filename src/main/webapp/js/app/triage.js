c2507e92a68e06f972743a12c77c4ee3d887f0c9
papito <Andrei Taranchenko drey10@gmail.com>
2017-06-09 06:35:14 -0400

    Initial non-active folder tree for triage.

--- src/main/webapp/js/app/triage.js
+++ src/main/webapp/js/app/triage.js
@@ -6,10 +6,61 @@ TriageViewModel = AssetsViewModel.extend({
 
     this.base();
     console.log('Initializing triage view model');
+
+    this.folders = ko.observableArray();
+    this.treeEl = $('#triageFolderTree');
+
+    /* Toggle between folder open and folder closed */
+    this.treeEl.on('open_node.jstree', function (e, data) {
+      data.instance.set_icon(data.node, "glyphicon glyphicon-folder-open"); });
+    this.treeEl.on('close_node.jstree', function (e, data) {
+      data.instance.set_icon(data.node, "glyphicon glyphicon-folder-close"); });
   },
 
-  setupFolderNav: function() {
+  loadFolders: function() {
     var self = this;
-    self.folders = ko.observableArray();
+
+
+    var opts = {
+      'successCallback': function (json) {
+        var hierarchy = json.hierarchy;
+        self.folders(hierarchy);
+
+        // traverse the hierarchy and "massage" the tree. name -> text
+        function _processFolderNode(node) {
+          node.text = node.name;
+          node.icon = "glyphicon glyphicon-folder-close";
+          node.id = 'triage_node_' + node.id;
+
+          for (var i = 0; i < node.children.length; ++i) {
+            var child = node.children[i];
+            _processFolderNode(child);
+          }
+        }
+
+        for (var i = 0; i < hierarchy.length; ++i) {
+          var node = hierarchy[i];
+          _processFolderNode(node);
+        }
+
+        $.jstree.defaults.core.themes.variant = "large";
+
+        self.treeEl.jstree({
+          'core' : {
+            "multiple" : false,
+            "animation" : 0,
+            "check_callback": true
+          },
+          "plugins" : ["search"]
+        });
+
+        self.treeEl.jstree(true).settings.core.data = hierarchy;
+        self.treeEl.jstree(true).refresh();
+
+      }
+    };
+
+    self.get('/api/v1/folders', opts);
+
   }
 });
