22bf8e9b595a20155edccb989a9406b166eae9b3
papito <Andrei Taranchenko drey10@gmail.com>
2017-08-05 15:24:41 -0400

    Admin view placeholders.

---
+++ src/main/webapp/js/app/admin.js
@@ -0,0 +1,10 @@
+AdminViewModel = BaseViewModel.extend({
+  constructor : function() {
+    "use strict";
+
+    var self = this;
+
+    this.base();
+    console.log('Initializing admin view model');
+  }
+});
