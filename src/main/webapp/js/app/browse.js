c403c50182fbc39fbab9c6f1f0baba1c0e26031c
papito <Andrei Taranchenko drey10@gmail.com>
2017-08-19 11:24:20 -0400

    Nuking WEB assets from the Core project.

--- src/main/webapp/js/app/browse.js
+++
@@ -1,9 +0,0 @@
-BrowseViewModel = AssetsViewModel.extend({
-  constructor : function() {
-    "use strict";
-
-    this.base();
-    var self = this;
-    console.log('Initializing browse view model');
-  }
-});
