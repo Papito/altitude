8220015754f5562cfd3c65de63f52b67b8e99ee8
Andrei Taranchenko <drey10@gmail.com>
2024-06-20 13:45:07 -0400

    Minor SQL formatting.

--- src/main/scala/software/altitude/core/dao/postgres/AssetDao.scala
+++ src/main/scala/software/altitude/core/dao/postgres/AssetDao.scala
@@ -10,8 +10,8 @@ import software.altitude.core.{Const => C}
 object AssetDao {
     val DEFAULT_SQL_COLS_FOR_SELECT: List[String] = List(
       "asset.*",
-      s"(asset.${C.Asset.METADATA}#>>'{}')::text as ${C.Asset.METADATA}",
-      s"(asset.${C.Asset.EXTRACTED_METADATA}#>>'{}')::text as ${C.Asset.EXTRACTED_METADATA}",
+      s"(asset.${C.Asset.METADATA}#>>'{}')::text AS ${C.Asset.METADATA}",
+      s"(asset.${C.Asset.EXTRACTED_METADATA}#>>'{}')::text AS ${C.Asset.EXTRACTED_METADATA}",
     )
 }
 
@@ -20,7 +20,7 @@ class AssetDao(app: AltitudeAppContext) extends software.altitude.core.dao.jdbc.
 
   override def getMetadata(assetId: String): Option[Metadata] = {
     val sql = s"""
-      SELECT (${C.Asset.METADATA}#>>'{}')::text as ${C.Asset.METADATA}
+      SELECT (${C.Asset.METADATA}#>>'{}')::text AS ${C.Asset.METADATA}
          FROM $tableName
        WHERE ${C.Base.REPO_ID} = ? AND ${C.Asset.ID} = ?
       """
