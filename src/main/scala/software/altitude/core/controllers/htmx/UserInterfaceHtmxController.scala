82a71d7b2b5c6671ca2075dcab6cd6dac9a758a6
Andrei Taranchenko <drey10@gmail.com>
2024-07-05 14:14:22 -0400

    Folder-add modal sends a request.

--- src/main/scala/software/altitude/core/controllers/htmx/UserInterfaceHtmxController.scala
+++ src/main/scala/software/altitude/core/controllers/htmx/UserInterfaceHtmxController.scala
@@ -4,7 +4,7 @@ import software.altitude.core.controllers.BaseHtmxController
 
 class UserInterfaceHtmxController extends BaseHtmxController{
 
-  get("/add-folder-modal") {
+  get("/modals/add-folder") {
     val minWidth: Int= params.get("minWidth") match {
       case Some(minWidth) => minWidth.toInt
       case None => 0
@@ -13,6 +13,10 @@ class UserInterfaceHtmxController extends BaseHtmxController{
     ssp("htmx/add_folder_modal",
       "minWidth" ->minWidth,
       "title" -> "Add Folder")
+  }
+
+  post("/folder/add") {
+    halt(200, "LOL")
   }
 
   // This just clears the body of the modal
