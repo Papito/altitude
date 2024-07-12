211aa4ba7e5ee01e3958f7e8ae10f4e93e25aaa8
Andrei Taranchenko <drey10@gmail.com>
2024-07-12 08:36:27 -0400

    PHASE 2. Mostly clean event for folder management, but the +/- state is not correctly implemented.

--- src/main/webapp/js/constants.js
+++ src/main/webapp/js/constants.js
@@ -3,11 +3,14 @@ export const Const = {
         folderMoved: "FOLDER_MOVED_EVENT",
         folderDeleted: "FOLDER_DELETED_EVENT",
         folderAdded: "FOLDER_ADDED_EVENT",
+        folderStateChanged: "FOLDER_STATE_CHANGED_EVENT",
+        folderCollapsed: "FOLDER_COLLAPSED_EVENT",
     },
 
     attributes: {
         expanded: "expanded",
         // CAREFUL! This is still hard-coded, once, in the root folder element
         isRoot: "is-root",
+        numOfChildren: "num-of-children",
     }
 }
