299782ed605a98aa59ecace205170c75f70fc542
Andrei Taranchenko <drey10@gmail.com>
2024-07-17 11:38:54 -0400

    There is some sort of "training" happening.

--- src/main/scala/research/FaceRecognition.scala
+++ src/main/scala/research/FaceRecognition.scala
@@ -1,19 +1,18 @@
 package research
 
 
-import org.apache.commons.io.FileUtils
+import org.apache.commons.io.{FileUtils, FilenameUtils}
 import org.bytedeco.javacpp.BytePointer
 import org.bytedeco.opencv.global.opencv_dnn.blobFromImage
 import org.bytedeco.opencv.global.opencv_dnn.readNetFromTorch
-import org.bytedeco.opencv.global.opencv_imgcodecs.IMREAD_ANYCOLOR
-import org.bytedeco.opencv.global.opencv_imgcodecs.imdecode
+import org.bytedeco.opencv.global.opencv_imgcodecs.{IMREAD_ANYCOLOR, imdecode, imwrite}
 import org.bytedeco.opencv.opencv_core.Mat
 import org.bytedeco.opencv.opencv_core.MatVector
 import org.bytedeco.opencv.opencv_core.Point
 import org.bytedeco.opencv.opencv_core.Rect
 import org.bytedeco.opencv.opencv_core.Scalar
 import org.bytedeco.opencv.opencv_core.Size
-import org.bytedeco.opencv.opencv_face.LBPHFaceRecognizer
+import org.bytedeco.opencv.opencv_face.{EigenFaceRecognizer, FisherFaceRecognizer, LBPHFaceRecognizer}
 import org.opencv.core.CvType
 import software.altitude.core.models.Face
 import software.altitude.core.service.FaceService
@@ -22,13 +21,14 @@ import java.io.File
 import java.nio.IntBuffer
 
 object FaceRecognition extends SandboxApp {
-  val recognizer: LBPHFaceRecognizer = LBPHFaceRecognizer.create()
+  private val recognizer  = FisherFaceRecognizer.create()
   private val modelFilePath = loadResourceAsFile("/opencv/openface_nn4.small2.v1.t7").getCanonicalPath
   private val embedder = readNetFromTorch(modelFilePath)
 
+  private var faceMats = List[Mat]()
+
   override def process(path: String): Unit = {
     val file = new File(path)
-
     val fileByteArray: Array[Byte] = FileUtils.readFileToByteArray(file)
     val image: Mat = imdecode(new Mat(new BytePointer(fileByteArray:_*)), IMREAD_ANYCOLOR)
 
@@ -37,13 +37,8 @@ object FaceRecognition extends SandboxApp {
     for (face <- faces) {
       val rect: Rect = new Rect(new Point(face.x1, face.y1), new Point(face.x2, face.y2))
       val faceBlob = blobFromImage(new Mat(image, rect), 1.0 / 255, new Size(96, 96), new Scalar(0, 0), true, false, CvType.CV_32F)
-      val images = new MatVector(1)
-      images.put(faceBlob)
-      val labels = new Mat(1, 1, CvType.CV_32SC1)
-      val labelsBuf: IntBuffer = labels.createBuffer()
-      labelsBuf.put(0, 0)
-//      recognizer.train(images, labels)
-      //      embedder.setInput(faceBlob)
+      faceMats = faceMats :+ faceBlob
+//      embedder.setInput(faceBlob)
 //      val embeddingsMat = embedder.forward()
 //      val embeddings = new Array[Float](128)
 //      embeddingsMat.get(0, 0, embeddings)
@@ -58,4 +53,15 @@ object FaceRecognition extends SandboxApp {
   }
 
   allFilePaths.foreach(process)
+
+  val faceBlobs = new MatVector(faceMats.size)
+  val labels = new Mat(faceMats.size, 1, CvType.CV_32SC1)
+  val labelsBuf: IntBuffer = labels.createBuffer()
+
+  for (idx <- faceMats.indices) {
+    faceBlobs.put(idx, faceMats(idx))
+    labelsBuf.put(idx, idx)
+  }
+
+  recognizer.train(faceBlobs, labels)
 }
