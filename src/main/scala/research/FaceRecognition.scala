8c0ec132a06c50e2f0e4f3377b9ee11746485d5c
Andrei Taranchenko <drey10@gmail.com>
2024-07-18 07:59:17 -0400

    Training face recognizer with org.opencv API.

--- src/main/scala/research/FaceRecognition.scala
+++ src/main/scala/research/FaceRecognition.scala
@@ -1,57 +1,50 @@
 package research
 
 
-import org.apache.commons.io.{FileUtils, FilenameUtils}
-import org.bytedeco.javacpp.BytePointer
-import org.bytedeco.opencv.global.opencv_dnn.blobFromImage
-import org.bytedeco.opencv.global.opencv_dnn.readNetFromTorch
-import org.bytedeco.opencv.global.opencv_imgcodecs.{IMREAD_ANYCOLOR, imdecode, imwrite}
-import org.bytedeco.opencv.opencv_core.Mat
-import org.bytedeco.opencv.opencv_core.MatVector
-import org.bytedeco.opencv.opencv_core.Point
-import org.bytedeco.opencv.opencv_core.Rect
-import org.bytedeco.opencv.opencv_core.Scalar
-import org.bytedeco.opencv.opencv_core.Size
-import org.bytedeco.opencv.opencv_face.{EigenFaceRecognizer, FisherFaceRecognizer, LBPHFaceRecognizer}
-import org.opencv.core.CvType
+import org.apache.commons.io.FileUtils
+import org.opencv.core
+import org.opencv.core.{CvType, Mat, MatOfByte, MatOfInt, Point, Rect, Scalar, Size}
+import org.opencv.dnn.Dnn.{blobFromImage, readNetFromTorch}
+import org.opencv.face.FisherFaceRecognizer
+import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgcodecs.Imgcodecs.imdecode
 import software.altitude.core.models.Face
 import software.altitude.core.service.FaceService
 
 import java.io.File
-import java.nio.IntBuffer
+import scala.jdk.CollectionConverters._
 
 object FaceRecognition extends SandboxApp {
   private val recognizer  = FisherFaceRecognizer.create()
   private val modelFilePath = loadResourceAsFile("/opencv/openface_nn4.small2.v1.t7").getCanonicalPath
   private val embedder = readNetFromTorch(modelFilePath)
 
-  private var faceMats = List[Mat]()
-
   override def process(path: String): Unit = {
     val file = new File(path)
     val fileByteArray: Array[Byte] = FileUtils.readFileToByteArray(file)
-    val image: Mat = imdecode(new Mat(new BytePointer(fileByteArray:_*)), IMREAD_ANYCOLOR)
+    val image: Mat = imdecode(new MatOfByte(fileByteArray:_*), Imgcodecs.IMREAD_ANYCOLOR)
 
     val faces: List[Face] = FaceService.detectFaces(fileByteArray)
 
-    for (face <- faces) {
+    for (idx <- faces.indices) {
+      val face = faces(idx)
       val rect: Rect = new Rect(new Point(face.x1, face.y1), new Point(face.x2, face.y2))
-      val faceBlob = blobFromImage(new Mat(image, rect), 1.0 / 255, new Size(96, 96), new Scalar(0, 0), true, false, CvType.CV_32F)
+      val faceBlob = blobFromImage(image.submat(rect), 1.0 / 255, new Size(96, 96), new Scalar(0, 0, 0), true, false)
+
       faceMats = faceMats :+ faceBlob
     }
   }
 
+  private var faceMats = List[Mat]()
   allFilePaths.foreach(process)
 
-  val faceBlobs = new MatVector(faceMats.size)
-  val labels = new Mat(faceMats.size, 1, CvType.CV_32SC1)
-  val labelsBuf: IntBuffer = labels.createBuffer()
+  // now that we know how many faces we have, we can create the labels
+  private val labels = new Mat(faceMats.length, 1, CvType.CV_32SC1);
 
   for (idx <- faceMats.indices) {
-    faceBlobs.put(idx, faceMats(idx))
-    labelsBuf.put(idx, idx)
+    labels.put (idx, 0, idx)
   }
 
-  println("Training recognizer...")
-  recognizer.train(faceBlobs, labels)
+  println("\nTraining recognizer...")
+  recognizer.train(faceMats.asJava, labels)
 }
