6706c70bfc53bee3907019cac5b2ad7a96b81be4
Andrei Taranchenko <drey10@gmail.com>
2024-07-26 15:33:18 -0400

    Totally working unsupervised learning for faces and people.

--- src/main/scala/research/FaceRecognition.scala
+++ src/main/scala/research/FaceRecognition.scala
@@ -57,14 +57,14 @@ object Person {
  * Note: A "trainable" person is a person with enough faces to train a model,
  * set with minTrainablePersonFaceNum
  */
-class Person(val label: Int, val isTrainable: Boolean = false) {
+class Person(val label: Int) {
   private val faces: mutable.TreeSet[PersonFace] = mutable.TreeSet[PersonFace]()
 
-  def addFaceAndGetUpdatedPerson(face: Face): Person = {
-    faces.addOne(new PersonFace(face, label))
+  def addFace(face: Face): PersonFace = {
+    val pFace = new PersonFace(face, label)
+    faces.addOne(pFace)
     println("Adding face " + face.name + " to person " + label + ". Number of faces: " + faces.size)
-
-    checkIfTrainableAndReturnNewPersonObj()
+    pFace
   }
 
   def name: String = {
@@ -78,47 +78,29 @@ class Person(val label: Int, val isTrainable: Boolean = false) {
     faces.toList
   }
 
-  def merge(person: Person): Person = {
+  def merge(person: Person): Unit = {
     println("Merging " + this.name + " with " + person.name)
     this.faces.addAll(person.allFaces())
-
-    checkIfTrainableAndReturnNewPersonObj()
-  }
-
-  private def checkIfTrainableAndReturnNewPersonObj(): Person = {
-    if (!isTrainable && faces.size >= Person.minTrainablePersonFaceNum) {
-      val trainablePerson = new Person(this.label, isTrainable = true)
-      trainablePerson.faces.addAll(this.faces)
-      return trainablePerson
-    }
-
-    // change nothing
-    this
   }
 
   override def toString: String =
-    s"PERSON $name. Label: $label. Faces: ${faces.size}. Trainable: $isTrainable"
+    s"PERSON $name. Label: $label. Faces: ${faces.size}"
 }
 
 
 object DB {
+  var LAST_LABEL = 1
+
   val db: mutable.Map[Int, Person] = mutable.Map[Int, Person]()
 
-  def allUntrainedPersonFaces(): List[PersonFace] = {
-    db.values.filter(!_.isTrainable).flatMap(_.allFaces()).toList
+  def allPersons(): List[Person] = {
+    db.values.toList
   }
 
-  def allTrainedPersons: Iterable[Person] = {
-    db.values.filter(_.isTrainable)
+  def allFaces(): List[PersonFace] = {
+    db.values.flatMap(_.allFaces()).toList
   }
 
-  def allUntrainedPersons: Iterable[Person] = {
-    db.values.filter(!_.isTrainable)
-  }
-
-  def all(): Iterable[Person] = {
-    db.values
-  }
 }
 
 object FaceRecognition extends SandboxApp {
@@ -183,7 +165,6 @@ object FaceRecognition extends SandboxApp {
     initialLabels.put(idx, 0, idx)
     InitialImages.add(alignedFaceImageGr)
   }
-
   recognizer.train(InitialImages, initialLabels)
 
   private val itr: Iterator[String] = recursiveFilePathIterator(sourceDirPath)
@@ -198,79 +179,105 @@ object FaceRecognition extends SandboxApp {
     process(path)
   }
 
-  private val cosineSimilarityThreshold = .5
+  private val cosineSimilarityThreshold = .45
 
-  private var labelIdx = 2
-
-  println("==>>>>>> INITIAL TRAINING RUN WITH MINIMAL DATA")
   srcFaces.forEach(thisFace => {
-
     breakable {
+
       println("\n--------------------\n" + thisFace.path + "\n")
-      labelIdx += 1
 
-      val predLabel = new Array[Int](1)
-      val confidence = new Array[Double](1)
-      recognizer.predict(thisFace.alignedFaceImageGraySc, predLabel, confidence)
-      println("Predicted label: " + predLabel(0) + " with confidence: " + confidence(0) + " for " + thisFace.name)
-      writeFrHit(predLabel = predLabel(0), confidence = confidence(0), face = thisFace)
+      // try to predict the label of the face
+      val predLabelArr = new Array[Int](1)
+      val confidenceArr = new Array[Double](1)
+      recognizer.predict(thisFace.alignedFaceImageGraySc, predLabelArr, confidenceArr)
+      val predLabel = predLabelArr.head
+      val confidence = confidenceArr.head
+      println("Predicted label: " + predLabel + " with confidence: " + confidence + " for " + thisFace.name)
 
-      val untrainedPersonFaceMatch: Option[PersonFace] = getUntrainedPersonFaceMatch(thisFace)
-
-      // found a match for this face for an existing person
-      if (untrainedPersonFaceMatch.isDefined) {
-        println("Found match for " + thisFace.name + " -> " + untrainedPersonFaceMatch.get.face.name)
-        val updatedPerson = DB.db(untrainedPersonFaceMatch.get.personLabel).addFaceAndGetUpdatedPerson(thisFace)
-        DB.db.put(updatedPerson.label, updatedPerson)
-        if (updatedPerson.isTrainable) {
-          val labels = new Mat(1, updatedPerson.allFaces().size, CvType.CV_32SC1)
-          val images = new util.ArrayList[Mat]()
-
-          println("Training person " + updatedPerson.name + " with " + updatedPerson.allFaces().size + " faces")
-          updatedPerson.allFaces().indices.foreach { idx =>
-            val personFace = updatedPerson.allFaces()(idx)
-            labels.put(idx, 0, personFace.personLabel)
-            images.add(personFace.face.alignedFaceImageGraySc)
-          }
-          recognizer.update(images, labels)
-        }
-
-      } else {
-        val person = new Person(label = labelIdx, isTrainable = false).addFaceAndGetUpdatedPerson(thisFace)
-        DB.db.put(labelIdx, person)
+      // if system label, ignore, we are probably at the start, just add the face and the NEW person
+      if (predLabel < 2) {
+        addNewPerson(thisFace)
+        break()
       }
 
-      println("Untrained persons " + DB.allUntrainedPersons.size)
-      println("Trained persons " + DB.allTrainedPersons.size)
+      // get the DB match and do a similarity check
+      val personMatch: Person = DB.db(predLabel)
+      println(s"Running similarity check for this face and the Recognizer match -> ${personMatch.name}")
+
+      comparisonOpCount += 1
+      val simScore = altitude.service.face.getFeatureSimilarityScore(
+        thisFace.features, personMatch.allFaces().head.face.features)
+      println("Similarity score: " + simScore)
+
+      if (simScore >= cosineSimilarityThreshold) {
+        println("MATCHED")
+        // Add the face and update the model
+        val personFace = personMatch.addFace(thisFace)
+        updateModelWithFace(personFace)
+        println("Faces in DB: " + DB.allFaces().size)
+        break()
+      }
+
+      // No match, brute force through all people (matching with only the top-ranking face)
+      val bestPersonFaceMatch: Option[PersonFace] = getPersonFaceMatches(thisFace)
+
+      // this is most likely a new person
+      if (bestPersonFaceMatch.isEmpty) {
+        addNewPerson(thisFace)
+      } else {
+        // WE GOT THEM
+        val matchedPerson = DB.db(bestPersonFaceMatch.get.personLabel)
+        val newPersonFace = matchedPerson.addFace(thisFace)
+        updateModelWithFace(newPersonFace)
+        println("Faces in DB: " + DB.allFaces().size)
+      }
+
+      //      writeFrHit(predLabel = predLabel(0), confidence = confidence(0), face = thisFace)
     }
   })
 
-  DB.all().foreach { person =>
-    println(person.name + " faces: \n" +  person.allFaces())
-    println()
-    writePerson(person)
-  }
-
-  //  println("==>>>>>> RERUNNING WITH TRAINED DATA")
-  //  srcFaces.forEach(thisFace => {
-  //      println("\n--------------------\n" + thisFace.path + "\n")
-  //
-  //      val predLabel = new Array[Int](1)
-  //      val confidence = new Array[Double](1)
-  //      recognizer.predict(thisFace.alignedFaceImageGraySc, predLabel, confidence)
-  //      writeFrHit(predLabel = predLabel(0), confidence = confidence(0), face = thisFace)
-  //  })
-
-  private def getUntrainedPersonFaceMatch(thisFace: Face): Option[PersonFace] = {
-    val unknownFaceSimilarityWeights: List[(Double, PersonFace)] = DB.allUntrainedPersonFaces().map { unknownPersonFace =>
-      comparisonOpCount += 1
-      val similarityScore = altitude.service.face.getFeatureSimilarityScore(
-        thisFace.features, unknownPersonFace.face.features)
-
-      (similarityScore, unknownPersonFace)
+    DB.allPersons().foreach { person =>
+      println(person.name + " faces: \n" +  person.allFaces())
+      println()
+      writePerson(person)
     }
 
-    val sortedMatchingSimilarityWeights = unknownFaceSimilarityWeights.sortBy(_._1)
+  private def addNewPerson(face: Face): Unit = {
+    println("Adding new person with face " + face.name)
+    val label = DB.LAST_LABEL + 1
+    val person = new Person(label = label)
+    val personFace: PersonFace = person.addFace(face)
+    DB.db.put(label, person)
+    updateModelWithFace(personFace)
+    DB.LAST_LABEL = label
+    println("Faces in DB: " + DB.allFaces().size)
+  }
+
+  private def updateModelWithFace(personFace: PersonFace): Unit = {
+    println("Updating model for person " + personFace.personLabel + " with " + personFace.face.name)
+    val labels = new Mat(1, 1, CvType.CV_32SC1)
+    val images = new util.ArrayList[Mat]()
+    labels.put(0, 0, personFace.personLabel)
+    images.add(personFace.face.alignedFaceImageGraySc)
+    recognizer.update(images, labels)
+  }
+
+  private def getPersonFaceMatches(thisFace: Face): Option[PersonFace] = {
+    val faceSimilarityScores: List[(Double, PersonFace)] = DB.allPersons().map { person =>
+      val bestFace = person.allFaces().head
+      println(s"Comparing ${thisFace.name} Q:${thisFace.detectionScore} with ${bestFace.face.name} Q:${bestFace.face.detectionScore}")
+      comparisonOpCount += 1
+      val similarityScore = altitude.service.face.getFeatureSimilarityScore(
+        thisFace.features, bestFace.face.features)
+      println(" -> " + similarityScore)
+
+      (similarityScore, bestFace)
+    }
+
+    // get the top one
+    val sortedMatchingSimilarityWeights = faceSimilarityScores.sortBy(_._1)
+//    println("WEIGHTS")
+//    println(sortedMatchingSimilarityWeights.mkString("\n"))
     val highestSimilarityWeights = sortedMatchingSimilarityWeights.filter(_._1 >= cosineSimilarityThreshold)
 
     highestSimilarityWeights.headOption match {
@@ -300,8 +307,7 @@ object FaceRecognition extends SandboxApp {
     person.allFaces().indices.foreach { idx =>
       val dbFace = person.allFaces()(idx)
       val ogFile = new File(dbFace.face.path)
-      val isCompleteStr = if (person.isTrainable) "compl" else "incompl"
-      val indexedFileName = isCompleteStr + "-" + personPrefix + "-" + idx + 1 + "." + FilenameUtils.getExtension(ogFile.getName)
+      val indexedFileName = personPrefix + "-" + idx + 1 + "." + FilenameUtils.getExtension(ogFile.getName)
       val outputPath = FilenameUtils.concat(outputDirPath, indexedFileName)
       println("Writing " + outputPath)
       Imgcodecs.imwrite(outputPath, dbFace.face.alignedFaceImage)
