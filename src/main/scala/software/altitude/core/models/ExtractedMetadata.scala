3c894f746886c05613936b47a28b2dc5926af79d
Andrei Taranchenko <drey10@gmail.com>
2024-11-08 12:38:43 -0500

    Tweaks and polish.

--- src/main/scala/software/altitude/core/models/ExtractedMetadata.scala
+++ src/main/scala/software/altitude/core/models/ExtractedMetadata.scala
@@ -5,24 +5,23 @@ import play.api.libs.json._
 import scala.language.implicitConversions
 
 object ExtractedMetadata {
-  type FieldValuesType = Map[String, String]
-  type MetadataType = Map[String, FieldValuesType]
+  private type FieldValuesType = Map[String, String]
+  private type MetadataType = Map[String, FieldValuesType]
 
   implicit def fromJson(json: JsValue): ExtractedMetadata = ExtractedMetadata(
     data = json.as[MetadataType]
   )
-
 }
 
 case class ExtractedMetadata(var data: ExtractedMetadata.MetadataType = Map[String, ExtractedMetadata.FieldValuesType]())
   extends BaseModel with NoId {
   /**
    * The raw extracted metadata is stored in a map of directories,
-   * each containing a list of field/value pairs.
+   * each containing a map of field/value pairs.
    *
    * Nikon Maker Note [Directory]
-   * Firmware Version [Field] = 2.10 [Value]
-   * ISO [Field] = ISO 125 [Value]
+   * ** Firmware Version [Field] = 2.10 [Value]
+   * ** ISO [Field] = ISO 125 [Value]
    */
 
   def addValue(directoryName: String, fieldName: String, value: String): Unit = {
