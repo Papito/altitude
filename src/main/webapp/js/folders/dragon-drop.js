689fa06cc8d73e2789683406ee04719400c0fea3
Andrei Taranchenko <drey10@gmail.com>
2024-11-20 07:51:57 -0500

    Simplifying dragon drop code for folders and people.

--- src/main/webapp/js/folders/dragon-drop.js
+++ src/main/webapp/js/folders/dragon-drop.js
@@ -1,78 +1,29 @@
-/**
- * Folder drag'n'drop functionality
- * ================================
- */
-import {Const} from "/js/constants.js";
+import {Const} from "/js/constants.js"
 import interact from "https://cdn.interactjs.io/v1.9.20/interactjs/index.js"
+import {dragged, dragMoveListener} from "/js/common/dragon-drop.js";
 
-/**
- * Maintain position of the dragged element in order to snap it back at cancel
- * https://github.com/taye/interact.js/issues/819#issuecomment-626599750
- */
-const dragState = {
-    x: 0,
-    y: 0,
-}
+interact('#rootFolderList .drag-drop').draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+        interact.modifiers.restrict({
+            restriction: '#rootFolderList > div.children',
+            endOnly: true,
+        })
+    ],
+    autoScroll: true,
 
-function dragged(e) {
-    dragState.x = 0;
-    dragState.y = 0;
-    e.target.style.transform = 'translate(0px, 0px)';
-
-    // reset the position attributes for draggables (on failed drag, it will resume from the last position)
-    e.target.removeAttribute("data-x")
-    e.target.removeAttribute("data-y")
-}
-
-interact('#rootFolderList .draggable')
-    .draggable({
-        // enable inertial throwing
-        inertia: true,
-        // keep the element within the area of it's parent
-        modifiers: [
-            interact.modifiers.restrictRect({
-                restriction: '#rootFolderList > div.children',
-                endOnly: true,
-                elementRect: {
-                    top: 0,
-                    left: 0,
-                    bottom: 1,
-                    right: 1
-                }
-            })
-        ],
-        // enable autoScroll
-        autoScroll: true,
-
-        listeners: {
-            move: dragMoveListener,
-            end: dragged,
-        }
-    })
-
-function dragMoveListener (event) {
-    dragState.x += event.dx;
-    dragState.y += event.dy;
-
-    event.target.style.transform = 'translate(' + dragState.x + 'px, ' + dragState.y + 'px)';
-
-    const target = event.target
-    // keep the dragged position in the data-x/data-y attributes
-    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-    // translate the element
-    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-    // update the position attributes
-    target.setAttribute('data-x', x)
-    target.setAttribute('data-y', y)
-}
+    listeners: {
+        move: dragMoveListener,
+        end: dragged,
+    }
+})
 
 // enable a draggable to be dropped into this
 interact('#rootFolderList .dropzone').dropzone({
     // only accept elements matching this CSS selector
-    accept: '#rootFolderList .draggable',
+    accept: '.drag-drop',
     // going above seems to break the dropzone
     overlap: 0.50,
 
@@ -107,12 +58,13 @@ interact('#rootFolderList .dropzone').dropzone({
         const movedFolderId = draggableElement.getAttribute("folder-id")
         const newParentFolderId = dropzoneElement.getAttribute("folder-id")
 
+        // console.log("DROPPED INTO FOLDER")
         const movedFolderEvent = new CustomEvent(
             Const.events.folderMoved, {
                 detail: {
                     movedFolderId: movedFolderId,
                     newParentId: newParentFolderId,
-    }});
+        }});
 
         document.body.dispatchEvent(movedFolderEvent)
     },
@@ -123,17 +75,3 @@ interact('#rootFolderList .dropzone').dropzone({
         event.target.classList.remove('drop-target')
     }
 })
-
-interact('#rootFolderList .drag-drop')
-    .draggable({
-        inertia: true,
-        modifiers: [
-            interact.modifiers.restrictRect({
-                restriction: '#rootFolderList .dropzone',
-                endOnly: true
-            })
-        ],
-        autoScroll: true,
-        // dragMoveListener from the dragging demo above
-        listeners: { move: dragMoveListener }
-    })
--- src/main/webapp/js/people/dragon-drop.js
+++ src/main/webapp/js/people/dragon-drop.js
@@ -1,40 +1,16 @@
 import {Const} from "/js/constants.js"
 import interact from "https://cdn.interactjs.io/v1.9.20/interactjs/index.js"
+import {dragged, dragMoveListener} from "/js/common/dragon-drop.js";
 
-/**
- * Maintain position of the dragged element in order to snap it back at cancel
- * https://github.com/taye/interact.js/issues/819#issuecomment-626599750
- */
-const dragState = {
-    x: 0,
-    y: 0,
-}
-
-function dragged(e) {
-    dragState.x = 0;
-    dragState.y = 0;
-    e.target.style.transform = 'translate(0px, 0px)';
-
-    // reset the position attributes for draggables (on failed drag, it will resume from the last position)
-    e.target.removeAttribute("data-x")
-    e.target.removeAttribute("data-y")
-}
-
-interact('#people .draggable')
+interact('#people .drag-drop')
     .draggable({
         // enable inertial throwing
         inertia: true,
         // keep the element within the area of it's parent
         modifiers: [
-            interact.modifiers.restrictRect({
+            interact.modifiers.restrict({
                 restriction: 'parent',
                 endOnly: true,
-                elementRect: {
-                    top: 0,
-                    left: 0,
-                    bottom: 1,
-                    right: 1
-                }
             })
         ],
         autoScroll: {container: document.getElementById('explorerViews')},
@@ -44,34 +20,13 @@ interact('#people .draggable')
         }
     })
 
-function dragMoveListener (event) {
-    dragState.x += event.dx;
-    dragState.y += event.dy;
-
-    event.target.style.transform = 'translate(' + dragState.x + 'px, ' + dragState.y + 'px)';
-
-    const target = event.target
-    // keep the dragged position in the data-x/data-y attributes
-    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-    // translate the element
-    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-    // update the position attributes
-    target.setAttribute('data-x', x)
-    target.setAttribute('data-y', y)
-}
-
 // enable draggables to be dropped into this
 interact('#people .dropzone').dropzone({
     // only accept elements matching this CSS selector
-    accept: '#people .draggable',
-    // going above seems to break the dropzone
+    accept: '#people .drag-drop',
     overlap: 0.50,
 
     // listen for drop related events:
-
     ondropactivate: function (event) {
         // add active dropzone feedback
         event.target.classList.add('drop-active')
@@ -98,6 +53,7 @@ interact('#people .dropzone').dropzone({
 
         draggableElement.classList.remove('can-drop')
 
+        // console.log("DROPPED INTO PERSON")
         const mergeSrcId = draggableElement.getAttribute("person-id")
         const mergeDestId = dropzoneElement.getAttribute("person-id")
 
@@ -117,17 +73,3 @@ interact('#people .dropzone').dropzone({
         event.target.classList.remove('drop-target')
     }
 })
-
-interact('#people .drag-drop')
-    .draggable({
-        inertia: true,
-        modifiers: [
-            interact.modifiers.restrictRect({
-                restriction: '#people .dropzone',
-                endOnly: true
-            })
-        ],
-        autoScroll: true,
-        // dragMoveListener from the dragging demo above
-        listeners: { move: dragMoveListener }
-    })
